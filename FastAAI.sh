f5627d8f46e78ad928ae1bb7805e30f0b3f984b6
Weizhi Song <songwz03@gmail.com>
2023-12-05 20:22:00 +0800

    -

--- FastAAI.sh
+++ FastAAI.sh
@@ -6,16 +6,11 @@ conda create --name fastaaienv
 conda activate fastaaienv
 pip install FastAAI
 
-fastaai build_db --genomes example_genomes/ --threads 4 --verbose --output example_build --database my_example_db.db --compress
-fastaai db_query --query example_build/database/my_example_db.db --target example_build/database/my_example_db.db --threads 4 --verbose --output example_build
-
 
 ################################## run FastAAI (on HPC3) #################################
 
 module load anaconda3
-conda activate ezaaienv
+conda activate fastaaienv
 cd /scratch/PI/ocessongwz/Tests/fastaai
-
 fastaai build_db --proteins Ting-tests --threads 4 --verbose --output fastaai_outputs --database user_genomes.db --compress
 fastaai db_query --query fastaai_outputs/database/user_genomes.db --target fastaai_outputs/database/user_genomes.db --threads 4 --verbose --output fastaai_outputs --output_style matrix
-
